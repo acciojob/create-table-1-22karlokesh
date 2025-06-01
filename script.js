d7310a3711955d0555f054f432cdf9787de6f7e9
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-06-01 17:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function insert_Row() {
     //Write your code here
 	let table = document.getElementById("sampleTable");
 
-	let newRow = table.insertRow();
+	let newRow = table.insertRow(0);
 	let cell1 = newRow.insertCell();
 	let cell2 = newRow.insertCell();
 
